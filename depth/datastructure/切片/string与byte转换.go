d85152143c29a3ac658a9ee08ccf249bb9a507e0
qiye45 <138199658+qiye45@users.noreply.github.com>
2025-08-13 00:06:48 +0800

    feat(depth): 添加字符串与字节切片转换的零拷贝实现
    - 新增 StringToBytes 和 StringToBytes2 函数，实现零拷贝转换
    - 添加示例代码，展示零拷贝转换的使用方法和效果
    -引入 reflect 和 unsafe 包，用于底层内存操作

--- depth/datastructure/切片/string与byte转换.go
+++ depth/datastructure/切片/string与byte转换.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
 func byteArrayToString(b []byte) string {
 	return string(b)
 }
@@ -9,6 +15,36 @@ func stringToByteArray(s string) []byte {
 }
 
 func main() {
+	//零拷贝公用内存
+	//s := "Hello"
+	// 这样生成的字符串来自堆上，不在只读段
+	s := string([]byte{'H', 'e', 'l', 'l', 'o'})
+	b := StringToBytes(s) // 零拷贝转换
+
+	fmt.Printf("原字符串: %q\n", s)
+	fmt.Printf("原字节切片: %v\n", string(b))
+
+	// 修改字节切片
+	b[0] = '1'
+	fmt.Println("修改字节切片后：")
+	fmt.Printf("字节切片: %v\n", string(b))
+	fmt.Printf("字符串: %q\n", s) // 会发现字符串内容也变了
 }
 
 //go tool compile -N -l -S main.go
+
+func StringToBytes(s string) (b []byte) {
+	sh := *(*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	bh.Data, bh.Len, bh.Cap = sh.Data, sh.Len, sh.Len
+	return b
+}
+
+func StringToBytes2(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(
+		&struct {
+			string
+			Cap int
+		}{s, len(s)},
+	))
+}
